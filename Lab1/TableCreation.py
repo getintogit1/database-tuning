510d70bb5995c855e9237cb77d9e2864452e43fe
getintogit1 <lukas.guenter@stud.plus.ac.at>
2025-03-19 15:21:07 +0100

    Initial commit with large files tracked by LFS

---
+++ Lab1/TableCreation.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+import psycopg2 as psy
+from Utils import dbSetup, dbname, user, password
+
+connection = dbSetup(dbname, user, password)
+def createTables(connection):
+    cursor = connection.cursor() 
+    cursor.execute("""
+    CREATE TABLE Auth (
+        name VARCHAR(49),
+        pubID VARCHAR(129)
+        );
+
+    CREATE TABLE Publ (
+        pubID VARCHAR(129),
+        type VARCHAR(13),
+        title VARCHAR(700),
+        booktitle VARCHAR(132),
+        year VARCHAR(4),
+        publisher VARCHAR(196)
+        );
+    """)
+    connection.commit()
+    cursor.close()
+    connection.close()
+
+    print("Created Table Auth and Publ complete.")
+
+createTables(connection)
---
+++ Lab1/TimeMeasure.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+import time
+import subprocess
+import psycopg2
+from Utils import dbname, password, user
+
+print(user)
+# Function to clear the tables 
+def clear_tables():
+    connection = psycopg2.connect(
+        host="localhost", 
+        dbname=dbname, 
+        user=user, 
+        password=password, 
+        port=5432
+    )
+    cursor = connection.cursor()
+    try:
+        cursor.execute("DELETE FROM auth;")
+        cursor.execute("DELETE FROM publ;")
+        connection.commit()
+        print("Tables cleared successfully.")
+    except Exception as e:
+        print(f"Error clearing tables: {e}")
+        connection.rollback()
+    finally:
+        cursor.close()
+        connection.close()
+
+# Here we are actually Measuring everything 
+def measure_execution_time(script_name):
+    start_time = time.time()  
+    try:        
+        subprocess.run(['python3', script_name], check=True)  
+    except subprocess.CalledProcessError as e:
+        print(f"Error running script {script_name}: {e}")
+        return None
+    end_time = time.time() 
+    
+    return end_time - start_time  # Return the time difference )
+
+def main():
+    clear_tables()
+
+    print("Running Naive.py")
+    naive_time = measure_execution_time('Naive.py')
+    print(f"Naive.py execution time: {naive_time:.4f} seconds")
+
+    clear_tables()
+
+    print("Running Tuned.py")
+    tuned_time = measure_execution_time('Tuned.py')
+    print(f"Tuned.py execution time: {tuned_time:.4f} seconds")
+
+if __name__ == "__main__":
+    main()
