a98069acb8dbcbec0416debdd0154749a036821f
David Ottino <david.ottino@stud.plus.ac.at>
2025-04-06 15:29:00 +0200

    New Query 1

--- Lab2/NaiveQuery.py
+++ Lab2/NaiveQuery.py
@@ -7,9 +7,15 @@ def make_naive_query(connection):
     try:
         cursor = connection.cursor() 
         query = """
-        SELECT Employee.ssnum
-        FROM Employee, Student
-        WHERE Employee.name = Student.name;        """
+        SELECT distinct E1.ssnum
+        FROM Employee E1, Techdept T
+        WHERE E1.salary between ((SELECT AVG(E2.salary)
+        FROM Employee E2, Techdept T
+        WHERE E2.dept = E1.dept
+        AND E2.dept = T.dept) - 1000) and ((SELECT AVG(E2.salary)
+        FROM Employee E2, Techdept T
+        WHERE E2.dept = E1.dept
+        AND E2.dept = T.dept) + 1000);        """
         cursor.execute(query)
         #printQueryResults(cursor, result_limit = 100)
     except Exception: 
--- Lab2/TunedQuery.py
+++ Lab2/TunedQuery.py
@@ -8,13 +8,26 @@ from Utils import dbSetup, dbname, printQueryResults, user, password
 def make_tuned_query(connection):
     try:
         cursor = connection.cursor() 
-        query = """
-            SELECT Employee.ssnum
-            FROM Employee, Student
-            WHERE Employee.ssnum = Student.ssnum;
+        avg_salary_query = """
+        SELECT AVG(E.salary)
+        FROM Employee E
+        JOIN Techdept T ON E.dept = T.dept
         """
-        cursor.execute(query)
-        #printQueryResults(cursor, result_limit = 100)
+        cursor.execute(avg_salary_query)
+        avg_salary = cursor.fetchone()[0]
+
+        query = """
+        SELECT DISTINCT E.ssnum
+        FROM Employee E
+        JOIN Techdept T ON E.dept = T.dept
+        WHERE E.salary BETWEEN %s AND %s
+        """
+        lower_bound = avg_salary - 1000
+        upper_bound = avg_salary + 1000
+
+        cursor.execute(query, (lower_bound, upper_bound))
+
+        printQueryResults(cursor, result_limit = 100)
     except Exception: 
         raise 
     finally:
