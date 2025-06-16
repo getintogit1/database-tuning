6c015c254f7e47a54edfbbd46e7f9e6111f79fa2
getintogit1 <146934493+getintogit1@users.noreply.github.com>
2025-06-16 16:18:07 +0200

    Update assignment_6_finished.sh

--- Lab6/Code/assignment_6_finished.sh
+++ Lab6/Code/assignment_6_finished.sh
@@ -1,22 +1,15 @@
 #!/bin/bash
 
-# Fail fast: Script stoppt bei Fehlern
-
-#!/bin/bash
-
-# Fail fast: Script stoppt bei Fehlern
 set -e
 
-# Konfiguration
 DB_USER="sam"
 DB_NAME="tuninglab6"
 
 echo "===== Creating database if not exists ====="
-# If database already exists, ignore error
 createdb -U $DB_USER $DB_NAME || echo "Database $DB_NAME already exists."
+
 echo "===== Creating the Accounts table ====="
 psql -U $DB_USER -d $DB_NAME -f create_accountDB.sql || echo "Table already exists!"
-
 
 echo "===== Solution A: Running main.py READ COMMITTED (workers 1-5) ====="
 for c in 1 2 3 4 5
@@ -24,6 +17,7 @@ do
     echo "----- Running Solution with $c concurrent workers (READ COMMITTED) -----"
     python3 main.py -t 100 -c $c -i read_committed -s a
 done
+
 echo "===== Solution A: Running main.py SERIALIZABLE (workers 1-5) ====="
 for c in 1 2 3 4 5
 do
@@ -31,22 +25,19 @@ do
     python3 main.py -t 100 -c $c -i serializable -s a
 done
 
-
 echo "===== Solution B: Running main.py READ COMMITTED (workers 1-5) ====="
 for c in 1 2 3 4 5
 do
     echo "----- Running Solution with $c concurrent workers (READ COMMITTED) -----"
     python3 main.py -t 100 -c $c -i read_committed -s b
 done
+
 echo "===== Solution B: Running main.py SERIALIZABLE (workers 1-5) ====="
 for c in 1 2 3 4 5
 do
     echo "----- Running with $c concurrent workers (SERIALIZABLE) -----"
     python3 main.py -t 100 -c $c -i serializable -s b
 done
-
-
-
 
 echo "===== DONE ====="
 
